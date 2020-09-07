9e2382e44960f9cd52c31dc72edf8e05273277f3
Mark Fadeev <mark.fadeev@ingrammicro.com>
2020-09-07 20:20:23 +0300

    init

---
+++ src/test/java/ru/fadeev/test/igaming/IgamingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fadeev.test.igaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
